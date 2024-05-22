a127d9d569bda03c54cecea376ae50670d427a64
Michael Gichuhi <gmikekamau@gmail.com>
2024-05-22 17:21:57 +0300

    added font, color and prettier

--- goGaming/src/App.tsx
+++ goGaming/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
-  return (
-    <h1 className="text-8xl font-bold underline">
-      Hello world
-    </h1>
-  )
+    return (
+        <h1 className="text-light_text font-urbanist text-2xl font-bold underline">
+            Hello world
+        </h1>
+    );
 }
